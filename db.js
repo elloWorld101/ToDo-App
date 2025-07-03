d25523c52d344199feec1af9810a7686c8e03cb2
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-03 16:55:39 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongooseUrl = require("../config");
+const mongoose = require("mongoose");
+
+console.log(mongooseUrl.mongooseUrl);
+mongoose.connect(mongooseUrl.mongooseUrl);
+
+const todoSchema = mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = {
+    Todo
+}
