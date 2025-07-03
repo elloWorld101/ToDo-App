649df73fc52617f1afecf46ac0500f2fd9932c0c
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-03 17:01:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const MainRouter = require("./routes/index");
+const MainRouter = require("./routes/main");
 
 app.use(express.json());
 app.use(cors());
