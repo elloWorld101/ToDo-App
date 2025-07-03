d1a16e35d490409c852455265eb7ec543fa53cbf
Rithvik bansal <157865151+elloWorld101@users.noreply.github.com>
2025-07-03 17:00:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const router = express.Router();
-const todoRouter = require("./todoRouter");
+const app = express();
+const cors = require("cors");
+const MainRouter = require("./routes/index");
 
-router.use("/todo", todoRouter);
+app.use(express.json());
+app.use(cors());
 
-module.exports = router;
+app.use("/api/v1", MainRouter);
+
+
+app.listen(3000, function(){
+    console.log("listening on port 3000");
+})
+
